27ec5cb3af7173125beaba5f68b28d99030a7014
adhito909 <adhito909@gmail.com>
2023-12-21 16:58:07 +0700

    Updated deploy script to include stopping, destroy container and stack naming

--- application-reactjs-with-nginx-multistage-build/deploy-container-nodejs-18-serve.sh
+++ application-reactjs-with-nginx-multistage-build/deploy-container-nodejs-18-serve.sh
@@ -1,1 +1,5 @@
-docker compose up
+docker-compose -p "nodejs-18-reactjs-app-compose" stop
+
+docker-compose -p "nodejs-18-reactjs-app-compose" down
+
+docker-compose -p "nodejs-18-reactjs-app-compose" up -d
