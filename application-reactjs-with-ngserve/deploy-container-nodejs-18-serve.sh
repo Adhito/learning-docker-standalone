62c6e69b38862078da5638f07d7a364542eb0035
adhito909 <adhito909@gmail.com>
2023-12-21 12:01:38 +0700

    Update and rename several folder

--- application-reactjs-with-ngserve/deploy-container-nodejs-18-serve.sh
+++
@@ -1,7 +0,0 @@
-docker stop nodejs-18-reactjs && docker rm nodejs-18-reactjs
-
-docker image rm repo.nexus.co.id:8082/nodejs-18-reactjs:1.0.0
-
-docker build --no-cache=true --load -t repo.nexus.co.id:8082/nodejs-18-reactjs:1.0.0 -f Dockerfile .
-
-docker run -d --name nodejs-18-reactjs -p 9000:9000 repo.nexus.co.id:8082/nodejs-18-reactjs:1.0.0
